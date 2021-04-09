35597388c2fad556c858920770d314030f6d3d3e
yuzu762 <76766143+yuzu762@users.noreply.github.com>
2021-04-09 19:22:12 +0800

    Create lock.cpp

---
+++ lock.cpp
@@ -0,0 +1,13 @@
+static inline void xspin_lock(volatile uint32_t *flag) {
+   while(__sync_lock_test_and_set(flag,1)){
+     while（*flag） {
+ #ifdef __amd64__ 
+        __asm__("pause" ::: "memory");
+ #endif
+ #ifdef __powerpc64__
+        __asm__("or 1, 1, 1" ::: "memory");
+           __asm__("or 2, 2, 2" ::: "memory");
+ #endif
+     }
+   }
+}
